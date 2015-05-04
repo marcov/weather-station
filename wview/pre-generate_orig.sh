c3c70670bd73041a2200c3890f5fa13c9cb9ff9e
marco v <marco@kotia.local>
2015-05-04 23:57:20 +0200

    renaming

---
+++ wview/pre-generate_orig.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+#cd /var/lib/wview/img
+#ORA=`date '+ %-H'`
+#MIN=`date '+ %-M'`
+#let ORA=ORA*100
+#TEMPO=$[$ORA+$MIN]
+#if [ $TEMPO -eq 0000 ] 
+#   then
+#   rm wview.txt
+#fi
+#if [ $TEMPO -ge 0008 ] && [ $TEMPO -le 2359 ]
+#   then
+#   cat wview.htm >> wview.txt
+#fi
+
+# Copy webcam image to the http-server served path
+#/bin/cp $IMAGE_FILE /var/lib/wview/img/webcam_1.jpg && /bin/chmod +r /var/lib/wview/img/webcam_1.jpg
+
