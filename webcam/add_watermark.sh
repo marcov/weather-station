dbe546de004612ccfadc33914fa478aef54d61d1
marco v <marco@kotia.local>
2015-05-04 23:52:59 +0200

    fix3

--- webcam/add_watermark.sh
+++ webcam/add_watermark.sh
@@ -5,6 +5,8 @@
 
 webcam_watermark_text="Fiobbio di Albino, `date +\"%d-%m-%Y  %T (%Z)\"`"
 
+echo "Watermark text will be: ${webcam_watermark_text}"
+
 #
 #
 #
@@ -12,7 +14,7 @@ convert -pointsize 16 \
         -fill white \
         -undercolor black \
         -gravity northwest \
-        -draw "$webcam_watermark_text" \
+        -draw 'text 0,0 "${webcam_watermark_text}"' \
         ${wview_html_dir}/${wview_webcam_px_nowm_name} \
         ${wview_html_dir}/${wview_webcam_px_name} 
 
