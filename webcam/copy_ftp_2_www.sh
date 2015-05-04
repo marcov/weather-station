1b60d66e5909f3952ea6c747b7e6c4c4f4fd4656
marco v <marco@kotia.local>
2015-05-04 23:49:27 +0200

    fix2

--- webcam/copy_ftp_2_www.sh
+++ webcam/copy_ftp_2_www.sh
@@ -11,7 +11,7 @@
 last_webcam_px=$(ls -rt ${ftp_upload_dir} | tail -n 1) 
 [ -e "${ftp_upload_dir}/${last_webcam_px}" ] || exit $?
 echo "Detected last webcam px is: ${last_webcam_px}"
-/bin/cp ${ftp_upload_dir}/${last_webcam_px} ${wview_html_dir}/${wview_webcam_px_name} || exit $?
+/bin/cp ${ftp_upload_dir}/${last_webcam_px} ${wview_html_dir}/${wview_webcam_px_nowm_name} || exit $?
 chmod +rw ${wview_html_dir}/${wview_webcam_px_nowm_name} | tail -n 1 || exit $?
 echo "All is good!"
 exit 0
