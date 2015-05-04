69c962ca8aa79dc40c9852dd44bded57fa8f0c66
Raspebby Fiobbio <pi@fiobbio.(none)>
2015-05-04 23:56:31 +0200

    add new pre-generate.sh

--- wview/pre-generate.sh
+++ wview/pre-generate.sh
@@ -1,19 +1,8 @@
-#!/bin/sh
+#/bin/sh
 
-#cd /var/lib/wview/img
-#ORA=`date '+ %-H'`
-#MIN=`date '+ %-M'`
-#let ORA=ORA*100
-#TEMPO=$[$ORA+$MIN]
-#if [ $TEMPO -eq 0000 ] 
-#   then
-#   rm wview.txt
-#fi
-#if [ $TEMPO -ge 0008 ] && [ $TEMPO -le 2359 ]
-#   then
-#   cat wview.htm >> wview.txt
-#fi
+cd /home/pi/weather_station/webcam || exit $?
+./copy_ftp_2_www.sh || exit $?
+./add_watermark.sh  || exit $?
+cd -
 
-# Copy webcam image to the http-server served path
-#/bin/cp $IMAGE_FILE /var/lib/wview/img/webcam_1.jpg && /bin/chmod +r /var/lib/wview/img/webcam_1.jpg
-
+exit 0
