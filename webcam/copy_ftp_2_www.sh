57d13224cbd5e718a65f7eb1861deb1fe02cc372
Raspebby Fiobbio <pi@raspberry-fiobbio.com>
2015-05-16 20:03:07 +0200

    clean up misma support

--- webcam/copy_ftp_2_www.sh
+++ webcam/copy_ftp_2_www.sh
@@ -8,21 +8,26 @@
 #
 # NO NEED TO EDIT BELOW THIS!
 #
-last_webcam_px=$(ls -rt ${ftp_upload_dir}/2* | tail -n 1) 
-[ -e "${last_webcam_px}" ] || exit $?
-echo "Detected last webcam px is: ${last_webcam_px}"
-/bin/cp ${last_webcam_px} ${wview_html_dir}/${wview_webcam_px_nowm_name} || exit $?
-chmod +rw ${wview_html_dir}/${wview_webcam_px_nowm_name} | tail -n 1 || exit $?
-echo "All is good!"
 
-#
-# Copy Misma pic
-#
-last_webcam_px=$(ls -rt ${ftp_upload_dir}/${misma_webcam_px_name} | tail -n 1) 
-[ -e "${last_webcam_px}" ] || exit $?
-echo "Detected misma webcam px is: ${last_webcam_px}"
-/bin/cp ${last_webcam_px} ${wview_html_dir}/${misma_webcam_px_nowm_name} || exit $?
-chmod +rw ${wview_html_dir}/${misma_webcam_px_nowm_name} | tail -n 1 || exit $?
+for pix in ${wview_webcam_px_nowm_name} ${misma_webcam_px_nowm_name}
+do
+
+	if [ "${pix}" == "${misma_webcam_px_nowm_name}" ] 
+	then
+		last_webcam_px=$(ls -rt ${ftp_upload_dir}/${misma_webcam_px_name} | tail -n 1) 
+	else
+		last_webcam_px=$(ls -rt ${ftp_upload_dir}/2* | tail -n 1) 
+	fi
+	[ -e "${last_webcam_px}" ] || exit $?
+	echo "Detected last webcam px is: ${last_webcam_px}"
+	echo "Copying and chmoding pix..."
+	/bin/cp ${last_webcam_px} \
+		${wview_html_dir}/${wview_webcam_px_nowm_name} || exit $?
+	chmod +rw \
+	${wview_html_dir}/${wview_webcam_px_nowm_name} || exit $?
+	echo "Done"
+done
+
 echo "All is good!"
 
 exit 0
