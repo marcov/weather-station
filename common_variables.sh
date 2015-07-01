2e829302df29071e68c65245bd3e8961c25faa6c
marco v <marco@kotia.local>
2015-07-01 21:22:09 +0200

    New names

--- common_variables.sh
+++ common_variables.sh
@@ -9,6 +9,6 @@ wview_webcam_small_px_name=webcam_small.jpg
 #
 #
 misma_ftp_upload_dir=${misma_ftp_upload_dir}/misma
-misma_webcam_px_nowm_name=webcampx_nowm.jpg
-misma_webcam_px_name=webcampx.jpg
-misma_webcam_small_px_name=webcampx_small.jpg
+misma_webcam_px_nowm_name=webcam_misma_nowm.jpg
+misma_webcam_px_name=webcam_misma.jpg
+misma_webcam_small_px_name=webcam_misma_small.jpg
