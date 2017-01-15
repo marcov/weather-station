cbb7069a62b14113a9f0330880d5e9f49969c962
marcov <marcov@fiobbio.local>
2017-01-15 17:06:48 +0100

    updates

--- deployment/pre-generate.sh
+++ deployment/pre-generate.sh
@@ -4,6 +4,6 @@
 #
 # Vedi: https://github.com/marcov/weather_station
 #
-/home/pi/weather_station/wview/pre-generate.sh || exit $?
+/home/pi/weather_station/wview/pre-generate.sh >/dev/null || exit $?
 
 exit 0
