2ad0b52edc30a2c53d2e27bd7e96dad4b70deab0
marcov <marcov@fiobbio.local>
2017-01-22 20:28:04 +0100

    New script all

--- wview/pre-generate.sh
+++ wview/pre-generate.sh
@@ -9,6 +9,9 @@ echo "Executing copy ftp 2 www..."
 echo "Executing add watermark..."
 ./add_watermark.sh && echo "OK!" || echo "Failed!" 
 
+echo "Uploading webcam imgs to CML ftp..."
+./upload_2_cml_ftp.sh && echo "OK!" || echo "Failed!" 
+
 echo "Executing wview.txt generate..." 
 ../wview/generate_wview_txt.sh && echo "OK!" || echo "Failed!"
 
