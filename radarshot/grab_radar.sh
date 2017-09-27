0464929aa2d69cc22878616baab31c4b14f66838
marcov <marcov@fiobbio.local>
2017-09-27 19:42:43 +0200

    Updated script post generate + grab radar

---
+++ radarshot/grab_radar.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+if which phantomjs
+then 
+    phantomjs_bin=phantomjs
+else
+    phantomjs_bin=/home/pi/bin/phantomjs-2.0.1-development-linux-armv6l/bin/phantomjs
+fi
+
+${phantomjs_bin} takeshot.js \
+    http://www.metradar.ch/2009/pc/index_mobile15.php \
+    10000 \
+    radar_ch.png \
+    1024x1024 
+    500x150x710x680 || exit $?
+
+${phantomjs_bin} takeshot.js \
+    http://cml.to/radar \
+    30000 \
+    radar_lom.png \
+    1024x800 \
+    0x20x960x800
