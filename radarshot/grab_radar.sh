5ba1870f4758ede2ae710d164fd20d682900de79
marcov <marcov@fiobbio.local>
2017-10-14 14:45:17 +0200

    Adding meteoguru

--- radarshot/grab_radar.sh
+++ radarshot/grab_radar.sh
@@ -1,4 +1,13 @@
-#!/bin/sh
+#!/bin/bash
+
+dst_path=/var/lib/wview/img
+#dst_path=.
+params=(\
+    "http://www.meteoguru.com/en/pro/ecmwf/?latlon=45.80,9.80 1000  ${dst_path}/mg_ecmwf.png  1280x1280  645x297x945x585" \
+    "http://www.metradar.ch/2009/pc/index_mobile15.php 10000 ${dst_path}/radar_ch.png 1024x1024 500x150x710x680" \
+    \
+    "http://cml.to/radar 30000 ${dst_path}/radar_lom.png 1024x800 0x20x960x800"\
+)
 
 if which phantomjs
 then 
@@ -7,16 +16,8 @@ else
     phantomjs_bin=/home/pi/bin/phantomjs-2.0.1-development-linux-armv6l/bin/phantomjs
 fi
 
-${phantomjs_bin} takeshot.js \
-    http://www.metradar.ch/2009/pc/index_mobile15.php \
-    10000 \
-    /var/lib/wview/img/radar_ch.png \
-    1024x1024 \
-    500x150x710x680 || exit $?
+for p in "${params[@]}"
+do
+     ${phantomjs_bin} takeshot.js ${p} || exit $?
+done 
 
-${phantomjs_bin} takeshot.js \
-    http://cml.to/radar \
-    30000 \
-    /var/lib/wview/img/radar_lom.png \
-    1024x800 \
-    0x20x960x800
