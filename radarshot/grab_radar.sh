7e71808909ec2c0a44795e88b1cebbe8869c51f7
marcov <marcov@fiobbio.local>
2017-10-16 10:10:49 +0200

    add param info + fixed zoomradar

--- radarshot/grab_radar.sh
+++ radarshot/grab_radar.sh
@@ -4,8 +4,10 @@ dst_path=/var/lib/wview/img
 #dst_path=.
 params=(\
     "http://www.meteoguru.com/en/pro/ecmwf/?latlon=45.80,9.80 1000  ${dst_path}/mg_ecmwf.png  1280x1280  645x297x945x585" \
+    \
     "http://www.metradar.ch/2009/pc/index_mobile15.php 10000 ${dst_path}/radar_ch.png 1024x1024 500x150x710x680" \
-    "http://www.zoomradar.ch/index.php?lon=9.866164648468462&lat=45.77935393888881 10000 ${dst_path}/radar_ch_detailed.png 1015x822 361x303x713x466" \
+    \
+    "http://www.zoomradar.ch/index.php?lon=9.866164648468462&lat=45.77935393888881 10000 ${dst_path}/radar_ch_detailed.png 1015x822 356x141x713x466" \
     \
     "http://cml.to/radar 30000 ${dst_path}/radar_lom.png 1024x800 0x20x960x800"\
 )
--- radarshot/takeshot.js
+++ radarshot/takeshot.js
@@ -21,28 +21,28 @@ function getArgs() {
   if (system.args.length === 1 || system.args[1] === "-h" || system.args[1] === "--help") {
     console.log("Usage: phantomjs" + system.args[0] +
                 " URL [waitTime] [outfile] [viewsize] [cropsize]");
-    console.log("[viewsize]: WIDTHxHEIGHT");
-    console.log("[cropsize]: TOPxLEFTxWIDTHxHEIGHT");
+    console.log("[viewsize]: _width_x_height_");
+    console.log("[cropsize]: _offsetRow_x_offsetColumn_x_width_x_height_");
     phantom.exit();
   }
-  
+
   args.url = system.args[1];
-  
+
   if (system.args.length > 2) {
     args.waitTime = system.args[2];
   }
-  
+
   if (system.args.length > 3) {
     args.outFile = system.args[3];
   }
   if (system.args.length > 4) {
     var viewSize = system.args[4].split("x");
-    
+
     if (viewSize.length !== 2) {
       console.log("Invalid viewsize format");
       phantom.exit();
     }
-    
+
     tmp = [];
     viewSize.forEach(function (i) {
       var n = parseInt(i);
@@ -50,11 +50,11 @@ function getArgs() {
         console.log("Invalid viewsize format");
       }
       tmp.push(n);
-      
+
     });
     args.viewSize = tmp;
   }
-  
+
   if (system.args.length > 5) {
     var cropSize = system.args[5].split("x");
     if (cropSize.length !== 4) {
@@ -68,11 +68,11 @@ function getArgs() {
         console.log("Invalid viewsize format");
       }
       tmp.push(n);
-      
+
     });
     args.cropSize = tmp;
   }
- 
+
   console.log("args:\n" + JSON.stringify(args));
 
   return args;
@@ -90,7 +90,7 @@ function takeShot(page, outFile) {
   });
 
   console.log(evalResult);
-  
+
   page.render(outFile);
   console.log("Screenshot done");
   phantom.exit();
@@ -109,10 +109,10 @@ function main() {
                    left: args.cropSize[1],
                    width: args.cropSize[2],
                    height: args.cropSize[3]};
- 
+
   //console.log(JSON.stringify(page.viewportSize));
   //console.log(JSON.stringify(page.clipRect));
-  
+
   page.onResourceRequested = function (request) {
     //console.log('Request ' + JSON.stringify(request, undefined, 4));
     console.log("+++ ask " + request.url);
@@ -126,9 +126,9 @@ function main() {
     console.log("page Load Finished");
     setTimeout(takeShot, args.waitTime, page, args.outFile);
   };
-  
+
   page.open(args.url, function (status) {
-    
+
     if (status !== "success") {
       console.log(">>> page open FAILED!");
       phantom.exit();
