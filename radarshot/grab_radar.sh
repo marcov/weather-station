fa49d8aaa248bbd7d0f5977cdf3b73599ed4ddd9
marcov <marcov@fiobbio.local>
2017-10-17 15:19:02 +0200

    Adding meteogram wz

--- radarshot/grab_radar.sh
+++ radarshot/grab_radar.sh
@@ -10,6 +10,8 @@ params=(\
     "http://www.zoomradar.ch/index.php?lon=9.866164648468462&lat=45.77935393888881 10000 ${dst_path}/radar_ch_detailed.png 1015x822 356x141x713x466" \
     \
     "http://cml.to/radar 30000 ${dst_path}/radar_lom.png 1024x800 0x20x960x800"\
+    \
+    "http://www.wetterzentrale.de/en/show_diagrams.php?geoid=76406&model=gfs&var=93&run=0&lid=OP&bw= 10000 wz_meteogram.png 820x1180 620x0x820x557"
 )
 
 if which phantomjs
