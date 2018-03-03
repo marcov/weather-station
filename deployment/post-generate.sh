7d419729a2b191e31586197facff7a63bfb93bd3
Raspebby Fiobbio <pi@raspberry-fiobbio.com>
2018-03-03 16:11:24 +0100

    Reorg

--- deployment/post-generate.sh
+++
@@ -1,9 +0,0 @@
-#/bin/sh
-
-# Esegue lo script dal repository weather_station.
-#
-# Vedi: https://github.com/marcov/weather_station
-#
-/home/pi/weather_station/wview/scripts/post-generate.sh >/dev/null || exit $?
-
-exit 0
---
+++ wview/etc_wiew/post-generate.sh
@@ -0,0 +1,10 @@
+#/bin/sh                                      
+
+cd /home/pi/weather_station/wview/scripts || exit $?                                         
+
+echo "Executing copy ftp 2 www..."            
+./cml_upload.sh && echo "OK"|| echo "Failed"  
+
+cd -                                          
+
+exit 0                       
