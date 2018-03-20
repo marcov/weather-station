230a60fa12ffb7791ca9013b45ba43be40a3663e
Raspebby Fiobbio <pi@raspberry-fiobbio.com>
2018-03-20 11:45:21 +0100

    tmp misma branch

--- wview/scripts/pre-generate.sh
+++ wview/scripts/pre-generate.sh
@@ -2,12 +2,12 @@
 
 cd /home/pi/weather_station/webcam || exit $?
 
-echo "Executing copy ftp 2 www..."
-./copy_ftp_2_www.sh && echo "OK!"|| echo "Failed!" 
+#echo "Executing copy ftp 2 www..."
+#./copy_ftp_2_www.sh && echo "OK!"|| echo "Failed!" 
 
 
-echo "Executing add watermark..."
-./add_watermark.sh && echo "OK!" || echo "Failed!" 
+#echo "Executing add watermark..."
+#./add_watermark.sh && echo "OK!" || echo "Failed!" 
 
 echo "Executing wview.txt generate..." 
 ../wview/scripts/generate_wview_txt.sh && echo "OK!" || echo "Failed!"
