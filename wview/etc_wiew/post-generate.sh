e5bffe6949fb1f8d78ea0233b32efd266d12a13f
marcov <mv@fiobbiorp3.lan>
2018-03-26 10:10:56 +0200

    Add verbose logger

--- wview/etc_wiew/post-generate.sh
+++ wview/etc_wiew/post-generate.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
-cd /home/pi/weather_station/wview/scripts >/dev/null || exit $?
-echo "Generating wview txt..."
-./generate_wview_txt.sh >/dev/null && echo "OK!" || echo "Failed!"
+cd /home/pi/weather_station/wview/scripts > /dev/null || exit $?
 
-echo "Executing cml upload..."
-./cml_upload.sh >/dev/null && echo "OK"|| echo "Failed"
+loggerTag="wview post-generate.sh"
+echo logger -t ${loggerTag} "Generating wview txt..."
+
+./generate_wview_txt.sh > /dev/null && logger -t ${loggerTag} "Exit code: $?"
+
+logger -t ${loggerTag} "CML ftp upload"
+./cml_upload.sh > /dev/null && logger -t ${loggerTag} "Exit code: $?"
 
 cd - >/dev/null
 
