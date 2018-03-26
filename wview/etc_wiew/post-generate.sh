b6e0ef40dea31cc3bcd1e6c5482e01e44c08075b
marcov <mv@fiobbiorp3.lan>
2018-03-26 09:11:27 +0200

    again...fixeddddd

--- wview/etc_wiew/post-generate.sh
+++ wview/etc_wiew/post-generate.sh
@@ -1,12 +1,12 @@
-#/bin/sh                                      
+#!/bin/sh
 
-cd /home/pi/weather_station/wview/scripts >/dev/null || exit $?                                         
-#echo "Generating wview txt..."
-./generate_wview_txt.sh >/dev/null #&& echo "OK!" || echo "Failed!"
+cd /home/pi/weather_station/wview/scripts >/dev/null || exit $?
+echo "Generating wview txt..."
+./generate_wview_txt.sh >/dev/null && echo "OK!" || echo "Failed!"
 
-#echo "Executing cml upload..."
-./cml_upload.sh >/dev/null #&& echo "OK"|| echo "Failed"  
+echo "Executing cml upload..."
+./cml_upload.sh >/dev/null && echo "OK"|| echo "Failed"
 
-cd - >/dev/null                                       
+cd - >/dev/null
 
-exit 0                       
+exit 0
