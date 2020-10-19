ea2fd32fe6d7829c85f4641586b9b0ccaec08ebd
fiobbio-rpi3 <me@fiobbiorp3.lan>
2020-10-19 21:41:59 +0200

    wview container working

---
+++ containers/run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+docker run \
+    --net=host --privileged -d --rm \
+    -v /var/lib/wview/archive:/var/lib/wview/archive \
+    -v /var/lib/wview/img:/var/lib/wview/img \
+    -v /var/lib/wview/conf:/var/lib/wview/conf \
+    -v /etc/wview:/etc/wview \
+    -v /home/pi/weather_station:/home/pi/weather_station \
+    -v/dev/log:/dev/log \
+    -v /etc/timezone:/etc/timezone:ro \
+    -v /etc/localtime:/etc/localtime:ro \
+    \
+    --name=wview \
+    \
+    pullme/wview:5.21.7 \
+    \
+    sh -c "/etc/init.d/wview restart; while true; do sleep 9999; done"
