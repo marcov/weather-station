876aeffca937d1cc66d12e6e2e264ac91791e01c
fiobbio-rpi3 <me@fiobbiorp3.lan>
2020-10-19 22:03:38 +0200

    fixes for containerized run

--- containers/run.sh
+++ containers/run.sh
@@ -6,6 +6,8 @@ docker run \
     -v /var/lib/wview/img:/var/lib/wview/img \
     -v /var/lib/wview/conf:/var/lib/wview/conf \
     -v /etc/wview:/etc/wview \
+    -v /etc/cml_ftp_login_data.sh:/etc/cml_ftp_login_data.sh:ro \
+    -v /etc/webcam_login_data.sh:/etc/webcam_login_data.sh:ro \
     -v /home/pi/weather_station:/home/pi/weather_station \
     -v/dev/log:/dev/log \
     -v /etc/timezone:/etc/timezone:ro \
