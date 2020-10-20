ec3b6b4db3de68a2e0c430d77ec813f09d835e86
fiobbio-rpi3 <me@fiobbiorp3.lan>
2020-10-20 20:15:14 +0200

    better wview post-generate.sh logging

--- wview/etc_wiew/post-generate.sh
+++ wview/etc_wiew/post-generate.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
+#
+# NOTE:
+# chronic will _only_ print to stdout if the command fails.
+# When that happens, send stdout to logger.
 
-#set -uo pipefail
+set -euo pipefail
 
-pushd /home/pi/weather_station/wview/scripts > /dev/null || exit $?
+declare -r scriptsPath="/home/pi/weather_station/wview/scripts"
+declare -r loggerTag="wview post-generate.sh"
 
-loggerTag="wview-post-generate.sh"
+pushd "${scriptsPath}" > /dev/null || { logger -t "${loggerTag}" "Failed to pushd scripts path: ${scriptsPath}"; exit $?; }
 
-logger -t ${loggerTag} "Generating wview txt..."
-chronic ./generate_wview_txt.sh | logger -t ${loggerTag}
+logger -t "${loggerTag}" "[start] generate wview txt"
+chronic ./generate_wview_txt.sh | logger -t "${loggerTag}"
 
-logger -t ${loggerTag} "CML FTP upload"
-chronic ./cml_upload.sh | logger -t ${loggerTag}
+logger -t "${loggerTag}" "[start] CML FTP upload"
+chronic ./cml_upload.sh | logger -t "${loggerTag}"
+logger -t "${loggerTag}" "[done] CML FTP upload"
 
 popd > /dev/null
 
