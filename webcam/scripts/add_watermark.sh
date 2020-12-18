4aabb14b91b373d57aac1aa46a5891482566c30c
fiobbio-rpi3 <me@fiobbiorp3.lan>
2020-12-18 10:30:23 +0100

    webcam fixes for SSL

--- webcam/scripts/add_watermark.sh
+++ webcam/scripts/add_watermark.sh
@@ -13,21 +13,28 @@ addWatermark() {
     ftplogin=$5
     resolution=$6
 
-    temperature="$(curl -fsSL "${temperatureUrl}" | jq -r .outsideTemp)"
+    #
+    # Add the insecure flag, since it's a local IP but it's using TLS.
+    #
+    temperature="$(curl -k -fsSL "${temperatureUrl}" | jq -r .outsideTemp)"
     stationName="$(sed "s/_/ /g" <<< "${stationName}")"
     dateTime="$(date +"%H:%M %d-%m-%Y")"
     watermarkText="${stationName}, ${temperature}°C - ${dateTime}"
 
     src=${wviewEphemeralImg}/${webcam_raw_prefix}_${name}.jpg
+
     dst=$(echo ${src} | sed "s/${webcam_raw_prefix}/${webcam_prefix}/g")
     dst_small=$(echo ${src} | sed "s/${webcam_raw_prefix}/${webcam_small_prefix}/g")
+
+    dst_tmp="${dst}.tmp"
+    dst_small_tmp="${dst_small}.tmp"
 
     [ -f "${src}" ] || { echo "ERR: ${src} does not exists, skip watermark"; return; }
 
     echo "SRC: ${src}"
     echo "  Watermark text:  ${watermarkText}"
-    echo "  DST (watermark): ${dst}"
-    echo "  DST (scaled):    ${dst_small}"
+    echo "  DST (watermark): ${dst_tmp}"
+    echo "  DST (scaled):    ${dst_small_tmp}"
 
     echo "Adding watermark"
     convert -pointsize 32 \
@@ -35,18 +42,19 @@ addWatermark() {
         -undercolor "rgba(0,0,0,0.6)" \
         -gravity northwest \
         -draw "text 0,0 \"${watermarkText}\" " \
-        ${src} ${dst}.tmp || return $?
+        ${src} ${dst_tmp} || return $?
     echo "Done"
 
     echo "Creating scaled image"
     convert \
         -resize ${resolution} \
-        ${dst} ${dst_small}.tmp || return $?
+        ${dst_tmp} ${dst_small_tmp} || return $?
     echo "Done"
 
-    mv ${dst}.tmp ${dst}
-    mv ${dst_small}.tmp ${dst_small}
-    rm -f ${src} ${dst}.tmp ${dst_small}.tmp
+    mv ${dst_tmp} ${dst}
+    mv ${dst_small_tmp} ${dst_small}
+
+    rm -f ${src} ${dst_tmp} ${dst_small_tmp}
 }
 
 addWatermark "${fiobbioCfg[@]}"
