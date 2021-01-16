9e98ec285c3a1f0322087adf35174906571ebfd7
fiobbio-rpi3 <me@fiobbiorp3.lan>
2021-01-16 10:12:03 +0100

    Fix webshots

--- webshot/run-in-container.sh
+++ webshot/run-in-container.sh
@@ -2,20 +2,31 @@
 
 set -euo pipefail
 
+declare configFile="config.json"
+
 . ../common_variables.sh
 
 declare scriptDir="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
+[ "$#" = 0 ] || configFile="$1"
+
+echo "INFO: using config file: ${configFile}"
+
 set -x
 
+# --add-host: HACK to avoid cookie consent popup
+# --add-host cdn.privacy-mgmt.com:0.0.0.0 \
+#
 docker run \
     --rm \
     \
-    -v "${scriptDir}"/config.json:/config.json \
+    -v "${scriptDir}/${configFile}":/config.json \
     -v "${wviewEphemeralImg}":/destdir \
     \
     -v /etc/timezone:/etc/timezone:ro \
     -v /etc/localtime:/etc/localtime:ro \
+    \
+    \
     \
     --name=webshot \
     \
