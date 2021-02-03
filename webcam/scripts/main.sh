790c5abca3e745a385209a05ec0ee0d7469b19d4
pi <nobody@nobody.fiobe>
2021-02-03 10:39:38 +0100

    webcam: better errmsg

--- webcam/scripts/main.sh
+++ webcam/scripts/main.sh
@@ -11,12 +11,12 @@ flock -n 999 || { echo "ERR: cannot lock $lockFile. Another instance of this scr
 declare -r scriptDir="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 echo "INFO: get raw image..."
-"${scriptDir}"/get_raw_image.sh || { echo "Failed to get raw image: \"$?\""; }
+"${scriptDir}"/get_raw_image.sh || { echo "ERR: Failed to get raw image: \"$?\""; }
 
 echo "INFO: add watermark..."
-"${scriptDir}"/add_watermark.sh || { echo "Failed to add watermark: \"$?\""; }
+"${scriptDir}"/add_watermark.sh || { echo "ERR: Failed to add watermark: \"$?\""; }
 
 echo "INFO: uploading 2 CML FTP..."
-"${scriptDir}"/upload_2_cml_ftp.sh || { echo "Failed to upload 2 CML FTP: \"$?\""; }
+"${scriptDir}"/upload_2_cml_ftp.sh || { echo "ERR: Failed to upload 2 CML FTP: \"$?\""; }
 
 exit 0
