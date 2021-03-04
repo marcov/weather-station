2b28612d93f5ce60e915c19d0e89542adaa56f73
pi <nobody@nobody.fiobe>
2021-03-04 23:06:59 +0100

    update wview

--- wview/scripts/k8s-startup.sh
+++ wview/scripts/k8s-startup.sh
@@ -2,11 +2,29 @@
 
 set -euo pipefail
 
+trap sighandler SIGINT SIGTERM ERR EXIT
+
+declare -i tailPid=
+declare -a wviewPids=
+
+sighandler() {
+    trap - SIGINT SIGTERM ERR EXIT
+    script cleanup here
+    echo "sighandler: exiting now!"
+
+    kill -KILL ${wviewPids[@]} $tailPid
+    exit 0
+}
+
 echo "k8s wview starting up..."
 
+# Set the timezone
 ln -sf /usr/share/zoneinfo/Europe/Rome /etc/localtime
 
+# If dev log is not provided, wait for the log socket from the syslog container
+# and symlink it to /dev/log
 if ! [ -e /dev/log ]; then
+
     while ! [ -e /tmp/syslog/dev-log.sock ]; do
         echo "Waiting for /dev/log from rsyslog container"
         sleep 1
@@ -31,24 +49,27 @@ sleep 1
 wvpmond &
 sleep 1
 
+tail -f /tmp/syslog/syslog &
+tailPid="$!"
+
 while true; do
-    pids=($(pgrep radmrouted; \
+    wviewPids=($(pgrep radmrouted; \
             pgrep "${wviewd_bin}"; \
             pgrep htmlgend; \
             pgrep wvhttpd; \
             pgrep wvpmond) \
          )
 
-    if [ ${#pids[@]} = 0 ]; then
+    if [ ${#wviewPids[@]} = 0 ]; then
         echo "INFO: No PIDs to wait, exiting..."
         break;
     fi
 
-    echo "INFO: Waiting PIDs: ${pids[@]}"
-    sleep 5
-done &
+    echo "INFO: Waiting PIDs: ${wviewPids[@]}"
+    sleep 60
+done
 
-tail -f /tmp/syslog/syslog
+kill -TERM "$tailPid"
 wait
 
 echo "k8s wview completed!"
