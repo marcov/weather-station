b7c968a817fb9e7319d6a4ebefaa89083483e78e
pi <nobody@nobody.fiobe>
2022-01-08 16:32:09 +0100

    update health check script

--- wview/scripts/health-check.sh
+++ wview/scripts/health-check.sh
@@ -2,19 +2,28 @@
 set -euo pipefail
 
 declare -i checkMinutes=2
-#checkDir="/tmp/wview-data-img/misma"
-checkDir="/var/lib/wview/img"
+#wviewImgDir="/tmp/wview-data-img/misma"
+wviewImgDir="/var/lib/wview/img"
+# NOTE: check a file we do not change afterwards :-)
+expectedFiles=( \
+    realtime.json \
+    tempdaycomp.png \
+)
 
 ###
 
-[ -d ${checkDir} ] || { echo "ERR: ${checkDir} is not a dir"; exit 2; }
+[ -d ${wviewImgDir} ] || { echo "ERR: ${wviewImgDir} is not a dir"; exit 2; }
 
-filesCount="$(find "${checkDir}" -mmin -${checkMinutes} | wc -l)"
+declare -i filesCount=0
+for f in ${expectedFiles[@]}; do
+    filesCount+="$(find "${wviewImgDir}" -mmin -${checkMinutes} -name "${f}" | wc -l)"
+done
 
 declare -i unhealthy=
-[ "${filesCount}" != 0 ] && unhealthy=0 || unhealthy=1
+[ "${filesCount}" = "${#expectedFiles[@]}" ] && unhealthy=0 || unhealthy=1
 
-sed -E -i "s,^unhealthy [01]$,unhealthy ${unhealthy}," "${checkDir}/metrics.prom"
+# Update health status
+sed -E -i "s,^unhealthy [01]$,unhealthy ${unhealthy}," "${wviewImgDir}/metrics.prom"
 
-echo "INFO: files count = ${filesCount} - unhealthy = ${unhealthy}"
+echo "INFO: expected files count = ${#expectedFiles[@]} - found = ${filesCount} - unhealthy = ${unhealthy}"
 [ "${unhealthy}" = 1 ] && exit 1 || exit 0
