bd9b71c8f3c346d1a1d0607b4e791abf6f43d4ea
pi <nobody@nobody.fiobe>
2022-01-29 20:39:11 +0100

    wview startup script tweaks

--- wview/scripts/k8s-startup.sh
+++ wview/scripts/k8s-startup.sh
@@ -9,13 +9,18 @@ declare -a wviewPids=
 
 sighandler() {
     trap - SIGINT SIGTERM ERR EXIT
-    echo "sighandler: trapped signal, exiting now!"
+    echo "INFO: sighandler: got signal, exiting now!"
+
+    while true; do
+        echo "WARN: keeping alive to allow debug"
+        sleep 1
+    done
 
     kill -KILL ${wviewPids[@]} $tailPid
     exit 0
 }
 
-echo "k8s wview starting up..."
+echo "INFO: k8s wview starting up..."
 
 # Set the timezone
 ln -sf /usr/share/zoneinfo/Europe/Rome /etc/localtime
@@ -25,15 +30,12 @@ ln -sf /usr/share/zoneinfo/Europe/Rome /etc/localtime
 if ! [ -e /dev/log ]; then
 
     while ! [ -e /tmp/syslog/dev-log.sock ]; do
-        echo "Waiting for /dev/log from rsyslog container"
+        echo "INFO: waiting for /dev/log from rsyslog container"
         sleep 1
     done
 
     ln -s /tmp/syslog/dev-log.sock /dev/log
 fi
-
-radmrouted 1 /var/lib/wview &
-sleep 2
 
 #wviewd_bin="/usr/bin/`cat /etc/wview/wview-binary`"
 wviewd_bin="/usr/bin/wviewd_${WVIEW_STATION_TYPE}"
@@ -42,30 +44,39 @@ if ! [ -x ${wviewd_bin} ]; then
     exit -1
 fi
 
+tail -n0 --follow --retry /tmp/syslog/syslog &
+tailPid="$!"
+
+radmrouted 1 /var/lib/wview &
+sleep 2
+echo "radmrouted PID: $(pidof -s radmrouted)"
+
 "${wviewd_bin}" &
 sleep 1
+echo "INFO: wviewd_${WVIEW_STATION_TYPE} PID: $(pidof -s wviewd_${WVIEW_STATION_TYPE})"
 
 htmlgend &
 sleep 1
+echo "INFO: htmlgend PID: $(pidof -s htmlgend)"
 
 wvhttpd &
 sleep 1
+echo "INFO: wvhttpd PID: $(pidof -s wvhttpd)"
 
 wvpmond &
 sleep 1
-
-tail -f /tmp/syslog/syslog &
-tailPid="$!"
+echo "INFO: wvpmond PID: $(pidof -s wvpmond)"
 
 while true; do
-    wviewPids=($(pgrep radmrouted; \
-            pgrep "${wviewd_bin}"; \
-            pgrep htmlgend; \
-            pgrep wvhttpd; \
-            pgrep wvpmond) \
-         )
+    wviewPids=( \
+        $(pidof -s radmrouted) \
+        $(pidof -s ${wviewd_bin}) \
+        $(pidof -s htmlgend) \
+        $(pidof -s wvhttpd) \
+        $(pidof -s wvpmond) \
+    )
 
-    if [ ${#wviewPids[@]} = 0 ]; then
+    if [[ ${#wviewPids[@]} = 0 ]]; then
         echo "INFO: No PIDs to wait, exiting..."
         break;
     fi
@@ -77,4 +88,4 @@ done
 kill -TERM "$tailPid"
 wait
 
-echo "k8s wview exiting"
+echo "INFO: k8s wview exiting"
