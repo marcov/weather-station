6b58698b0652dac6b145917cfef1189393050e7b
pi <nobody@nobody.fiobe>
2023-09-21 18:28:38 +0200

    minor clarification for letsencrypt

--- misc/letsencrypt-renew.sh
+++ misc/letsencrypt-renew.sh
@@ -2,9 +2,12 @@
 
 # Don't set -e: the script should run to completion so that nginx is always
 # restarted, even on error.
+set +e
+
 set -x -u -o pipefail
 
-kubectl delete service nginx || { echo "No nginx service, already deleted?"; }
+# service delte may fail, if it does not exist - just keep going.
+kubectl delete service nginx
 
 # dry run
 sudo certbot renew  --logs-dir /tmp --config-dir ~/secrets/letsencrypt --work-dir /tmp --standalone --dry-run
