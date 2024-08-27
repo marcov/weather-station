46b13bfd2a7e3e2f26f4fc2b13ba95fd3c585775
pi <nobody@nobody.fiobe>
2024-08-27 09:01:56 +0200

    letsencrypt tweaks

--- host/letsencrypt-renew.sh
+++ host/letsencrypt-renew.sh
@@ -6,14 +6,19 @@ set +e
 
 set -x -u -o pipefail
 
-# service delte may fail, if it does not exist - just keep going.
+# dump service
+tmp_service_file=$(mktemp /tmp/k8s_service_nginx.XXXX)
+kubectl get service -o yaml nginx > ${tmp_service_file}
+
+# service delete may fail, if it does not exist - just keep going.
 kubectl delete service nginx
 
 # dry run
-sudo certbot renew  --logs-dir /tmp --config-dir ~/secrets/letsencrypt --work-dir /tmp --standalone --dry-run
+#sudo certbot renew  --logs-dir /tmp --config-dir ~/secrets/letsencrypt --work-dir /tmp --standalone --dry-run
 
 # actual run
 sudo certbot renew  --logs-dir /tmp --config-dir ~/secrets/letsencrypt --work-dir /tmp --standalone
 
-kubectl apply -f k8s/manifests/nginx-no-ingress.yaml
+# restore & restart nginx
+kubectl apply -f ${tmp_service_file}
 kubectl delete pod -l app=nginx
