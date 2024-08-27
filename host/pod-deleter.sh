1e5f1eff299247f7229383145d9f384a2547b0ee
pi <nobody@nobody.fiobe>
2024-08-27 09:02:06 +0200

    comment pod deleter

--- host/pod-deleter.sh
+++ host/pod-deleter.sh
@@ -8,5 +8,6 @@ set -x
 podName=$(kubectl get pods -o=json | jq -r '.items[] | "\(.metadata.name) \(.status.containerStatuses[] | select(.restartCount>'${restartThreshold}').name)"' | awk '{print $1}')
 
 if [[ ${podName} != '' ]]; then
+    echo "Found pods with restart count higher than ${restartThreshold} ... deleting"
     kubectl delete pod ${podName}
 fi
