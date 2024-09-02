a08feff6dbda8c0bfccffd8899b3bc8e779b8403
pi <nobody@nobody.fiobe>
2024-09-02 14:13:32 +0200

    fix downloader url

--- apps/downloader/run.sh
+++ apps/downloader/run.sh
@@ -6,8 +6,13 @@
 set -euo pipefail
 
 declare -r destDir="/tmp/downloader"
-declare -r local_grafana_domain=nginx
-declare -r grafana_all_stations_rain_panel="https://${local_grafana_domain}/grafana/render/d-solo/Ewu-ii3Mk/wview-all-stations?orgId=1&from=now&to=now&theme=dark&width=250&height=200&tz=Europe%2FRome&panelId="
+#
+# NOTE: grafana URLs are only valid when running inside a K8s pod.
+# If running outside of a cluter, you'll need a different URL and path
+# (probably prefix with /grafana/render/...)
+#
+declare -r local_grafana_domain=grafana:3000
+declare -r grafana_all_stations_rain_panel="http://${local_grafana_domain}/render/d-solo/Ewu-ii3Mk/wview-all-stations?orgId=1&from=now&to=now&theme=dark&width=250&height=200&tz=Europe%2FRome&panelId="
 
 # Calculate the expected timestamp of the last sat image:
 # from current time, subtract 1h30m, then round to 15 minutes increments
@@ -30,9 +35,9 @@ echo "Calculate sat timestamp image TS is ${sat_timestamp}"
 declare -r sat_alps_url="https://imn-api.meteoplaza.com/v4/nowcast/tiles/satellite-europe/${sat_timestamp}/7/44/65/47/70?outputtype=jpeg"
 
 declare -A urls=( \
-    #[fiobbio-t-1m]="https://${local_grafana_domain}/grafana/render/d-solo/dqNwskkRz/wview-history?orgId=1&from=now-1m&to=now&panelId=9&width=1000&height=500&tz=Europe%2FRome"
-    #[fiobbio-t-7d]="https://${local_grafana_domain}/grafana/render/d-solo/dqNwskkRz/wview-history?orgId=1&from=now-7d&to=now&panelId=9&width=1000&height=500&tz=Europe%2FRome"
-    #[yearrain.png]="https://${local_grafana_domain}/grafana/render/d-solo/Ewu-ii3Mk/wview---all-stations?orgId=1&kiosk=tv&refresh=1m&from=1696843849094&to=1696930249094&panelId=40&width=250&height=380&tz=Europe%2FRome&theme=light&kiosk"
+    #[fiobbio-t-1m]="http://${local_grafana_domain}/render/d-solo/dqNwskkRz/wview-history?orgId=1&from=now-1m&to=now&panelId=9&width=1000&height=500&tz=Europe%2FRome"
+    #[fiobbio-t-7d]="http://${local_grafana_domain}/render/d-solo/dqNwskkRz/wview-history?orgId=1&from=now-7d&to=now&panelId=9&width=1000&height=500&tz=Europe%2FRome"
+    #[yearrain.png]="http://${local_grafana_domain}/render/d-solo/Ewu-ii3Mk/wview---all-stations?orgId=1&kiosk=tv&refresh=1m&from=1696843849094&to=1696930249094&panelId=40&width=250&height=380&tz=Europe%2FRome&theme=light&kiosk"
     [rain_day.png]="${grafana_all_stations_rain_panel}"43
     [rain_month.png]="${grafana_all_stations_rain_panel}"42
     [rain_year.png]="${grafana_all_stations_rain_panel}"40
